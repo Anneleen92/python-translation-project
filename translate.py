e05e4d59a40fb1940291195017737309da0a5ef4
Anneleen Swanepoel <anneleen.swanepoel@gmail.com>
2026-02-23 15:46:00 -0600

    Get the get_reverse function working

--- translate.py
+++ translate.py
@@ -75,7 +75,8 @@ def get_reverse(sequence):
     >>> get_reverse('AUGC')
     'CGUA'
     """
-    pass
+    return sequence.upper()[::-1]
+
 
 def get_complement(sequence):
     """Get the complement of a `sequence` of nucleotides.
